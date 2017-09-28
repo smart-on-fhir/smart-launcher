9be93887989fa2b8d3e61b9957e76f6e9f1cc237
Vladimir Ignatov <vladimir.ignatov@childrens.harvard.edu>
2017-09-28 12:10:37 -0400

    Generate some UI from config

---
+++ static/config.js
@@ -0,0 +1,48 @@
+var CONFIG = {
+    fhirVersions: [
+        {
+            name: "r2",
+            displayName: "R2 (DSTU2)",
+            tags: [
+                {
+                    id: "r2-smart",
+                    value: "smart-8-2017",
+                    displayName: "SMART",
+                    description: "Core SMART Sample Patients",
+                    selected: true
+                },
+                {
+                    id: "r2-synthea",
+                    value: "synthea-8-2017",
+                    displayName: "Synthea",
+                    description: "SMART Synthea Sample Patients"
+                }
+            ]
+        },
+        {
+            name: "r3",
+            displayName: "R3 (STU3)",
+            tags: [
+                {
+                    id: "r3-smart",
+                    value: "smart-7-2017",
+                    displayName: "SMART",
+                    description: "Core SMART Sample Patients",
+                    selected: true
+                },
+                {
+                    id: "r3-synthea",
+                    value: "synthea-7-2017",
+                    displayName: "Synthea",
+                    description: "SMART Synthea Sample Patients"
+                },
+                {
+                    id: "r3-pro",
+                    value: "pro-7-2017",
+                    displayName: "PRO",
+                    description: "SMART PRO Sample Patients"
+                }
+            ]
+        }
+    ]
+};
