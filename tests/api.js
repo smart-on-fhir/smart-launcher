17be625503248b46ee43b243025dc6770a687df2
Vladimir Ignatov <vladimir.ignatov@childrens.harvard.edu>
2017-09-29 13:08:47 -0400

    Added tests

---
+++ tests/api.js
@@ -0,0 +1,141 @@
+const request = require('supertest');
+const app     = require("../src/index.js");
+const config  = require("../src/config");
+const jwt     = require("jsonwebtoken");
+
+function buildRoutePermutations(suffix = "") {
+    suffix = suffix.replace(/^\//, "");
+    return [
+        `/v/r3/sb/abc/sim/whatever/${suffix}`, 
+        `/v/r3/sb/abc/${suffix}`,
+        `/v/r3/sim/whatever/${suffix}`,
+        `/v/r3/${suffix}`
+    ];
+}
+
+describe('index', function() {
+    it('responds with html', function(done) {
+        request(app)
+        .get('/')
+        .expect('Content-Type', /html/)
+        .expect(200, done);
+    });
+});
+
+["picker", "login", "authorize"].forEach(endPoint => {
+    describe(endPoint, function() {
+        buildRoutePermutations(endPoint).forEach(path => {
+            describe(`GET ${path}`, function() {
+                it('responds with html', function(done) {
+                    request(app)
+                    .get(path)
+                    .expect('Content-Type', /html/)
+                    .expect(200, done);
+                });
+            });
+        });
+    });
+});
+
+describe('Proxy', function() {
+    buildRoutePermutations("fhir/metadata").forEach(path => {
+        it(path + ' responds with json+fhir', done => {
+            request(app)
+            .get(path)
+            // .set('Accept', 'application/json')
+            .expect('Content-Type', /^application\/json\+fhir/)
+            .expect(200, done);
+        });
+        it(path + ' responds with html in browsers', done => {
+            request(app)
+            .get(path)
+            .set('Accept', 'text/html')
+            .expect('Content-Type', /^text\/html/)
+            .expect(200, done);
+        });
+
+        // TODO: Support XML
+        // it(path + ' responds with xml if requested', done => {
+        //     request(app)
+        //     .get(path)
+        //     .set('Accept', 'application/xml')
+        //     .expect('Content-Type', /^application\/xml\+fhir/)
+        //     .expect(200, done);
+        // });
+    });
+
+    it ("Validates the fhir version", done => {
+        request(app)
+        .get("/v/r300/fhir/metadata")
+        .expect('Content-Type', /json/)
+        .expect('{"error":"FHIR server r300 not found"}')
+        .expect(400, done);
+    });
+
+    it ("If auth token is sent - validates it", done => {
+        request(app)
+        .get("/v/r3/fhir/metadata")
+        .set("authorization", "Bearer whatever")
+        .expect('Content-Type', /text/)
+        .expect("Invalid token")
+        .expect(401, done);
+    });
+
+    it ("Can simulate custom token errors");
+    it ("Keeps protected data-sets read-only");
+    it ("Inject sandbox tag into POST and PUT requests");
+    it ("Make urls conditional and if exists, change /id to ?_id=");
+    it ("Apply patient scope to GET requests");
+
+    it ("Adjust urls in the fhir response", done => {
+        request(app)
+        .get("/v/r3/fhir/Patient")
+        .expect(res => {
+            if (res.text.indexOf(config.fhirServerR3) > -1) {
+                throw new Error("Not all URLs replaced");
+            }
+        })
+        .end(done);
+    });
+
+    it ("Inject the SMART information in metadata responses");
+    it ("pull the resource out of the bundle if we converted a /id url into a ?_id= query");
+    it ("Pretty print if called from a browser");
+
+    it ("Handles pagination", done => {
+        request(app)
+        .get("/v/r3/fhir/Patient")
+        .expect(res => {
+            if (!Array.isArray(res.body.link)) {
+                throw new Error("No links found");
+            }
+
+            let next = res.body.link.find(l => l.relation == "next")
+            if (!next) {
+                throw new Error("No next link found");
+            }
+            // console.log(next)
+            return request(app).get(next.url).expect(res2 => {
+                if (!Array.isArray(res.body.link)) {
+                    throw new Error("No links found on second page");
+                }
+
+                let self = res.body.link.find(l => l.relation == "self")
+                if (!self) {
+                    throw new Error("No self link found on second page");
+                }
+                if (self.url !== next.url) {
+                    throw new Error("Links mismatch");
+                }
+
+                let next2 = res.body.link.find(l => l.relation == "next")
+                if (!next2) {
+                    throw new Error("No next link found on second page");
+                }
+
+                console.log(next2)
+            })
+        })
+        .end(done);
+    });
+});
