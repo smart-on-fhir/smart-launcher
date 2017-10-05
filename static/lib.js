932458358041f1e6f3c42e0f0ee59920dd3b5b0a
Vladimir Ignatov <vladimir.ignatov@childrens.harvard.edu>
2017-10-05 15:59:32 -0400

    Added the getPath lib function

--- static/lib.js
+++ static/lib.js
@@ -2,6 +2,19 @@
 (function($, undefined) {
 
     /**
+     * Walks thru an object (ar array) and returns the value found at the
+     * provided path. This function is very simple so it intentionally does not
+     * support any argument polymorphism, meaning that the path can only be a
+     * dot-separated string. If the path is invalid returns undefined.
+     * @param {Object} obj The object (or Array) to walk through
+     * @param {String} path The path (eg. "a.b.4.c")
+     * @returns {*} Whatever is found in the path or undefined
+     */
+    function getPath(obj, path = "") {
+        return path.split(".").reduce((out, key) => out ? out[key] : undefined, obj)
+    }
+
+    /**
      * Parses the current query string and returns a key/value map of all the
      * parameters.
      * @todo: Handle ampersands 
@@ -189,6 +202,7 @@
     // =========================================================================
     
     const Lib = {
+        getPath          : getPath,
         getUrlQuery      : getUrlQuery,
         compileUrlQuery  : compileUrlQuery,
         toCamelCase      : toCamelCase,
