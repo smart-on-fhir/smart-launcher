50a96fcf1dd1fb1c2f1b8ff97f8f1606899b207c
Vladimir Ignatov <vladimir.ignatov@childrens.harvard.edu>
2017-10-13 12:08:39 -0400

    Add first run notification. Closes #29

--- static/lib.js
+++ static/lib.js
@@ -254,6 +254,45 @@
             .replace(/minutes?/, "min");
     }
 
+    /**
+     * Set (create or update) a cookie.
+     * @param {String} name The name of the cookie
+     * @param {*} value The value of the cookie
+     * @param {Number} days (optional) The cookie lifetime in days. If omitted,
+     *                                 the cookie is a session cookie.
+     * @return {void}
+     */
+    function setCookie( name, value, days ) {
+        if ( String(name).indexOf(";") > -1 ) {
+            throw "The cookie name cannot contain ';'";
+        }
+        var expires = "";
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + value + expires + "; path=/";
+    }
+
+    /**
+     * Reads a cookie identified by it's name.
+     * @param {String} name The name of the cookie
+     * @return {String|null} The value of the cookie or null on failure
+     */
+    function getCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') 
+                c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) === 0) 
+                return c.substring(nameEQ.length,c.length);
+        }
+        return null;
+    }
+
     // Export
     // =========================================================================
     
@@ -267,7 +306,9 @@
         humanName        : humanName,
         formatAge        : formatAge,
         qsToForm         : qsToForm,
-        formToQs         : formToQs
+        formToQs         : formToQs,
+        setCookie        : setCookie,
+        getCookie        : getCookie
     };
 
     // Export at window.Lib:
