30cabe1164112ad60359733ad6d3ab0fe8621116
Vladimir Ignatov <vladimir.ignatov@childrens.harvard.edu>
2017-11-13 17:13:47 -0500

    Simulated errors depend on the launch flow now

--- static/config.js
+++ static/config.js
@@ -45,5 +45,57 @@ var CONFIG = {
                 }
             ]
         }
+    ],
+    simulatedErrors: [
+        {
+            name : "auth_invalid_client_id",
+            label: "Authorize - Invalid Client Id",
+            for  : ["launch-ehr", "launch-prov", "launch-pt"]
+        },
+        {
+            name : "auth_invalid_redirect_uri",
+            label: "Authorize - Invalid Redirect Url",
+            for  : ["launch-ehr", "launch-prov", "launch-pt"]
+        },
+        {
+            name : "auth_invalid_scope",
+            label: "Authorize - Invalid Scope",
+            for  : ["launch-ehr", "launch-prov", "launch-pt"]
+        },
+        {
+            name : "auth_invalid_client_secret",
+            label: "Token - invalid client secret (for confidential clients)",
+            for  : ["launch-ehr", "launch-prov", "launch-pt"]
+        },
+        {
+            name : "token_invalid_token",
+            label: "Token - Invalid Token",
+            for  : ["launch-ehr", "launch-prov", "launch-pt", "launch-bs"]
+        },
+        {
+            name : "token_expired_refresh_token",
+            label: "Token - Expired Refresh Token",
+            for  : ["launch-ehr", "launch-prov", "launch-pt"]
+        },
+        {
+            name : "token_expired_registration_token",
+            label: "Token - Expired Registration Token (for backend services)",
+            for  : ["launch-bs"]
+        },
+        {
+            name : "invalid_jti",
+            label: "Token - Invalid jti value (for backend services)",
+            for  : ["launch-bs"]
+        },
+        {
+            name : "request_invalid_token",
+            label: "Request - Invalid Token",
+            for  : ["launch-ehr", "launch-prov", "launch-pt", "launch-bs"]
+        },
+        {
+            name : "request_expired_token",
+            label: "Request - Expired Token",
+            for  : ["launch-ehr", "launch-prov", "launch-pt", "launch-bs"]
+        }
     ]
 };
