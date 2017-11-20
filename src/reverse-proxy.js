7ca505023dc7506f3cfd816eab08ee4dd4ca059b
Vladimir Ignatov <vladimir.ignatov@childrens.harvard.edu>
2017-11-20 16:45:24 -0500

    test

--- src/reverse-proxy.js
+++ src/reverse-proxy.js
@@ -120,7 +120,7 @@ module.exports = function (req, res) {
         // special handler for metadata requests - inject the SMART information
         if (req.url.match(/^\/metadata/) && response.statusCode == 200 && body.indexOf("fhirVersion") != -1) {
             let authBaseUrl = sandboxify.buildUrlPath(config.baseUrl, req.baseUrl.replace(config.fhirBaseUrl, config.authBaseUrl));
-            let secure = req.secure && req.headers["x-forwarded-proto"];
+            let secure = req.secure && req.headers["x-forwarded-proto"] == "https";
             authBaseUrl = authBaseUrl.replace(/^https?/, secure ? "https" : "http");
             body = sandboxify.addAuthToConformance(body, authBaseUrl);
             if (!body) {
