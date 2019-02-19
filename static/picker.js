bfad639c3e9baf53163fb92af9ee6207a3cec641
Vladimir Ignatov <vladimir.ignatov@childrens.harvard.edu>
2019-02-19 11:26:08 -0500

    Various improvements and fix for #12

--- static/picker.js
+++ static/picker.js
@@ -1,30 +1,27 @@
 var SmartPicker = (function() {
 
     var state = {
-        skip: "0",
-        searchText: "",
-        mode: "loading",
-        pageSize: "10",
-        sortParam: "name", 
-        sortDir: "asc",
-        apiUrlSegment: "fhir",
+        skip          : 0,
+        searchText    : "",
+        mode          : "loading",
+        pageSize      : 10,
+        sortParam     : "name", 
+        sortDir       : "asc",
+        apiUrlSegment : "fhir",
         authUrlSegment: "auth/authorize"
     };
 
-    // UI Event Handlers
     function bindUiEvents() {
         $("#paging-next, #paging-previous").click(handlePagingClick);
         $("th").click(handleHeaderClick);
         $("#search-form").submit(handleSearchSubmit);
-        $("#search-text").focus()
         $("table").on("click", ".patient", handlePatientClick);
     }
 
     function handlePagingClick() {
         var dir = 1;
-        if ($(this).attr("id") == "paging-previous")
-            dir = -1;
-        state.skip = "" + (parseInt(state.skip) + parseInt(state.pageSize) * dir);
+        if ($(this).attr("id") == "paging-previous") dir = -1;
+        state.skip += state.pageSize * dir;
         loadFhir(dir == 1 ? state.links.next : state.links.previous)
     }
 
@@ -53,11 +50,10 @@ var SmartPicker = (function() {
                 }
             }
         } catch(ex) {
-            console.error(ex)
+            console.error(ex);
         }
-        if (state.showIds != "1" || e.target.tagName == "BUTTON") {
-            launchApp(patientId);
-        }
+
+        launchApp(patientId);
     }
 
     function handleSearchSubmit(e) {
@@ -66,25 +62,42 @@ var SmartPicker = (function() {
         loadFhir();
     }
 
-    // AJAX
+    function buildQueryString(params) {
+        var result = [];
+        for (var name in params) {
+            result.push(name + "=" + encodeURIComponent(String(params[name])));
+        }
+        return result.join("&");
+    }
+
     function buildFhirUrl() {
+        var query = {
+            _format : "application/json+fhir",
+            _summary: true,
+            _count  : state.pageSize
+        };
+
         var sortParam = state.sortParam;
-        var sortDir = state.sortDir;
+        var sortDir   = state.sortDir;
 
         if (sortParam == "age") {
-            sortParam = "birthdate"
-            sortDir = (sortDir == "asc") ? "desc" : "asc";
+            sortParam = "birthdate";
+            sortDir   = (sortDir == "asc") ? "desc" : "asc";
         } else if (sortParam == "name") {
-            sortParam = "family"
+            sortParam = "family";
         }
 
-        // return state.apiUrlSegment + 
-        return state.aud + 
-            "/Patient/?_format=application/json+fhir&_summary=true" +
-            "&_count=" + state.pageSize +
-            (state.patient ? "&_id=" + state.patient.replace(/\s*/g, "") : "") + 
-            (sortParam ? "&_sort:" + sortDir + "=" + sortParam : "") +
-            (state.searchText != "" ? "&name:contains=" + encodeURIComponent(state.searchText) : "");
+        query["_sort:" + sortDir] = sortParam;
+
+        if (state.patient) {
+            query._id = state.patient.replace(/\s*/g, "");
+        }
+
+        if (state.searchText) {
+            query["name:contains"] = state.searchText;
+        }
+
+        return state.aud + "/Patient/?" + buildQueryString(query);
     }
 
     function getLinks(data) {
@@ -106,23 +119,22 @@ var SmartPicker = (function() {
         $.get(fhirUrl)
             .done( function(data) {
                 state.data = data;
-                state.mode = "data"
+                state.mode = "data";
                 state.links = getLinks(data);
                 render();
             })
             .fail(function() {
-                state.mode = "error"
+                state.mode = "error";
                 render();
             });
     }
 
-    // Launcher
     function launchApp(patientId) {
         
         var url = window.location.href
             .replace("picker", state.authUrlSegment)
             .replace(/(&?)patient=[^&]*/g, "") + 
-            "&patient=" + window.encodeURIComponent(patientId);
+            "&patient=" + encodeURIComponent(patientId);
 
         
         if (state.newWindow == "1") {
@@ -132,17 +144,19 @@ var SmartPicker = (function() {
         }
     }
 
-    // Renderer Helpers
-    function buildPatientRow(patient, tpl, showIds, hideButton) {
+    function buildPatientRow(patient, tpl, hideButton) {
         return tpl
             .replace("{id}", patient.id)
-            .replace("{name}", formatName(patient, showIds))
+            .replace("{name}", Lib.humanName(patient))
             .replace("{showButton}", hideButton ? "none" : "inline")
             .replace("{gender}", formatGender(patient))
             .replace("{age}", formatAge(patient))
     }
 
     function formatAge(patient) {
+        if (patient.deceasedBoolean == "true")
+            return '<small" class="label label-warning">Deceased</small">';
+        
         var dob = patient.birthDate;
         if (!dob) return "";
         
@@ -152,26 +166,26 @@ var SmartPicker = (function() {
         if (/\d{4}-d{2}$/.test(dob))
             dob = dob + "-01"
 
-        return moment(dob).fromNow(true)
-            .replace("a ", "1 ")
-            .replace(/minutes?/, "min");
+        var age = moment(dob).from(patient.deceasedDateTime || moment(), true);
+        return age.replace("a ", "1 ").replace(/minutes?/, "min") +
+            (patient.deceasedDateTime ?
+                ' <small" class="label label-warning">Deceased</small">' :
+                ""
+            );
     }
 
     function formatGender(patient) {
-        var gender = patient.gender || "unknown";
-        return gender[0].toUpperCase();
+        var gender = String(patient.gender || "-").charAt(0).toUpperCase();
+        if (gender == "M") return '<span class="male">M</span>';
+        if (gender == "F") return '<span class="female">F</span>';
+        return '<span style="color:#666" title="' + patient.gender + '">' + gender + '</span>';
     }
 
-    function formatName(patient, showIds) {
-        return Lib.humanName(patient) + (showIds ? " [" + patient.id + "]" :  "");
-    }
-
-    // Renderer
     function render() {
         function _renderError() {
             if (state.errorMessage) $("#global-error-message").text(message);
-            $(".picker").hide()
-            $("#global-error").show()
+            $(".picker").hide();
+            $("#global-error").show();
         }
 
         function _renderTableSortIndicators() {
@@ -193,18 +207,18 @@ var SmartPicker = (function() {
         function _renderPatients() {
             var tpl = $("#patient-row-template").html();
             patientRows = $.map(state.data.entry, function(entry) {
-                return buildPatientRow(entry.resource, tpl, state.showIds == "1", state.launchUrl == "")
+                return buildPatientRow(entry.resource, tpl, state.launchUrl == "")
             });
             $(".picker table > tbody").prepend(patientRows.join(""))
             $("#message-loading").hide();
         }
 
         function _renderPaging() {
-            $("#paging-from").text(parseInt(state.skip)+1);
-            $("#paging-to").text(parseInt(state.skip) + state.data.entry.length);
-            $("#paging-total").text(state.data.total);
-            $("#paging-next").toggle(state.data.total > parseInt(state.skip) + state.data.entry.length);
-            $("#paging-previous").toggle(parseInt(state.skip) > 0);
+            $("#paging-from").text(state.skip + 1);
+            $("#paging-to").text(state.skip + state.data.entry.length);
+            $("#paging-total").text(state.data.total ? " of " + state.data.total : "");
+            $("#paging-next").toggle(!!state.links.next);
+            $("#paging-previous").toggle(!!state.links.previous);
             $("#paging").show();
         }
 
@@ -229,7 +243,7 @@ var SmartPicker = (function() {
                 state.aud = location.href.split("?")[0].replace(/\/picker$/, "/fhir");
             }
             bindUiEvents();
-            $(".container").show()
+            $(".container").show();
             loadFhir();
         }
     }
